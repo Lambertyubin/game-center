c9ad58a0f4f10ec59166f504c9a2d510654b7c05
Lambertyubin <lambertyubin@gmail.com>
2023-06-25 12:24:40 +0100

    filter games by genre

---
+++ src/services/helpers.ts
@@ -0,0 +1,8 @@
+export const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export const getResizedTextLength = (text: string) =>
+  text.length > 11 ? text.slice(0, 11) + ".." : text;
